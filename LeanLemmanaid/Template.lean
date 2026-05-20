6c35540f7de608e95a08e8cd0b250c36a54b3fbb
Dattabhasvant Ghadiyaram <dattabhasvan@iisc.ac.in>
2026-05-20 15:51:51 +0200

    Fixed elaboration for correct IR

--- LeanLemmanaid/Template.lean
+++ LeanLemmanaid/Template.lean
@@ -17,7 +17,7 @@ def stxCheck (id : Syntax) (pfx : String) : MetaM Nat := do
 inductive tempLit
   | var : Nat → tempLit
   | opHole : Nat → Array tempLit → tempLit
-  | typeHole : Nat → tempLit
+  --| typeHole : Nat → tempLit
 deriving Repr
 
 inductive tempUnOp
@@ -34,26 +34,25 @@ instance : Repr tempUnOp where
 inductive tempBinOp
  | and |or | imp
 
-instance : ToString tempBinOp where
-  toString
-    | .and => "and"
-    | .or  => "or"
-    | .imp => "imp"
+-- instance : ToString tempBinOp where
+--   toString
+--     | .and => "and"
+--     | .or  => "or"
+--     | .imp => "imp"
 
--- 2. The Pretty Printer (What #eval sees)
 instance : Repr tempBinOp where
   reprPrec
-    | .and, _ => "∧"
-    | .or,  _ => "∨"
-    | .imp, _ => "→"
+    | .and, _ => "And"
+    | .or,  _ => "Or"
+    | .imp, _ => "Imp"
 
 inductive tempBinder
   | forallBind | existsBind
 
 instance : Repr tempBinder where
   reprPrec
-  | .forallBind, _ => "∀"
-  | .existsBind, _ => "∃"
+  | .forallBind, _ => "Forall"
+  | .existsBind, _ => "Exists"
 
 
 inductive tempExpr
@@ -64,10 +63,10 @@ inductive tempExpr
   | bind : tempBinder → Nat → tempExpr → tempExpr
 deriving Repr
 
-structure template where
-  opHoles : Array Nat
-  vars : Array Nat
-  statement : tempExpr
+-- structure template where
+--   opHoles : Array Nat
+--   vars : Array Nat
+--   statement : tempExpr
 
 -- Syntax for templates
 
@@ -87,25 +86,27 @@ syntax temp_lit_atom temp_lit_atom+ : temp_lit
 
 mutual    -- Functions that call each other
 /- Function to elaborate atoms, i.e. x1, (x1), etc.. -/
-  partial def elabAtom (stx : Syntax) : MetaM Expr := do
+  partial def elabAtom (stx : Syntax) : MetaM tempLit := do
     match stx with
     | `(temp_lit_atom| $id:ident) =>
       let nameStr := id.getId.toString
       -- If it is x"num" then make it a var
       if nameStr.startsWith "x" then
         let k ← stxCheck id "x"
-        mkAppM ``tempLit.var #[mkNatLit k]
+        -- mkAppM ``tempLit.var #[mkNatLit k]
+        return tempLit.var k
 
       -- H"num" -> Operator Hole
       else if nameStr.startsWith "H" then
         let n ← stxCheck id "H"
-        let emptyArray ← mkAppM ``Array.empty #[.const ``tempLit []]
-        mkAppM ``tempLit.opHole #[mkNatLit n, emptyArray]
+        -- let emptyArray ← mkAppM ``Array.empty #[.const ``tempLit []]
+        -- mkAppM ``tempLit.opHole #[mkNatLit n, emptyArray]
+        return tempLit.opHole n #[]
 
       -- T"num" -> Type hole
-      else if nameStr.startsWith "T" then
-        let n ← stxCheck id "T"
-        mkAppM ``tempLit.typeHole #[mkNatLit n]
+      -- else if nameStr.startsWith "T" then
+      --   let n ← stxCheck id "T"
+      --   mkAppM ``tempLit.typeHole #[mkNatLit n]
 
       else
         throwErrorAt id s!"Unknown Lemmanaid identifier prefix for '{nameStr}'. Expected x, H, or T."
@@ -114,7 +115,7 @@ mutual    -- Functions that call each other
     | `(temp_lit_atom| ( $inner:temp_lit ) ) => elabLit inner
     | _ => throwUnsupportedSyntax
 
-  partial def elabLit (stx : Syntax) : MetaM Expr := do
+  partial def elabLit (stx : Syntax) : MetaM tempLit := do
     match stx with
     | `(temp_lit| $atm:temp_lit_atom) => elabAtom atm
     | `(temp_lit| $fnAtom:temp_lit_atom $args:temp_lit_atom*) => do
@@ -126,11 +127,10 @@ mutual    -- Functions that call each other
           let mut argExprs := #[]
           for arg in args do
             argExprs := argExprs.push (← elabAtom arg)
-          let listExpr ← mkListLit (.const ``tempLit []) argExprs.toList
-          let arrExpr ← mkAppM ``List.toArray #[listExpr]
-
-          -- Scrutinize this later
-          mkAppM ``tempLit.opHole #[mkNatLit opIdx, arrExpr]
+          -- let listExpr ← mkListLit (.const ``tempLit []) argExprs.toList
+          -- let arrExpr ← mkAppM ``List.toArray #[listExpr]
+          -- mkAppM ``tempLit.opHole #[mkNatLit opIdx, arrExpr]
+          return tempLit.opHole opIdx argExprs
         else
           throwErrorAt id "Application head must be an operator starting with 'H' (e.g., H1)"
 
@@ -138,12 +138,12 @@ mutual    -- Functions that call each other
     | _ => throwUnsupportedSyntax
 end
 
-elab "test_tempLit " l:temp_lit : term => elabLit l
+-- elab "test_tempLit " l:temp_lit : term => elabLit l
 
-#eval test_tempLit x1
-#eval test_tempLit H1 x1 x2
-#eval test_tempLit H1 (H2 x1 x2) (H3 x2 x1 x3)
-#eval test_tempLit T5
+-- #eval test_tempLit x1
+-- #eval test_tempLit H1 x1 x2
+-- #eval test_tempLit H1 (H2 x1 x2) (H3 x2 x1 x3)
+-- #eval test_tempLit T5
 
 -- Elaborating Expressions
 
@@ -151,8 +151,8 @@ declare_syntax_cat temp_unop
 syntax "not " : temp_unop
 syntax "¬ " : temp_unop
 
-def elabUnOp : Syntax → MetaM Expr
-  | `(temp_unop| not) => return .const ``tempUnOp.not []
+def elabUnOp : Syntax → MetaM tempUnOp
+  | `(temp_unop| not) => return .not
   | _ => throwUnsupportedSyntax
 
 declare_syntax_cat temp_binop
@@ -166,17 +166,16 @@ syntax " ∨ " : temp_binop
 syntax " imp " : temp_binop
 syntax "→" : temp_binop
 
-macro_rules
-| `(temp_unop| ¬)  => `(temp_unop| not)
+-- macro_rules
+--   | `(temp_unop| ¬)  => `(temp_unop| not)
+--   | `(temp_binop| ∧)  => `(temp_binop| and)
+--   | `(temp_binop| ∨)  => `(temp_binop| or)
+--   | `(temp_binop| →)  => `(temp_binop| imp)
 
-  | `(temp_binop| ∧)  => `(temp_binop| and)
-  | `(temp_binop| ∨)  => `(temp_binop| or)
-  | `(temp_binop| →)  => `(temp_binop| imp)
-
-def elabBinOp : Syntax → MetaM Expr
-  | `(temp_binop| and) | `(temp_binop| ∧) => return .const ``tempBinOp.and []
-  | `(temp_binop| or)  | `(temp_binop| ∨)=> return .const ``tempBinOp.or []
-  | `(temp_binop| imp) | `(temp_binop| →)=> return .const ``tempBinOp.imp []
+def elabBinOp : Syntax → MetaM tempBinOp
+  | `(temp_binop| and) | `(temp_binop| ∧) => return .and
+  | `(temp_binop| or)  | `(temp_binop| ∨)=> return .or
+  | `(temp_binop| imp) | `(temp_binop| →)=> return .imp
   | _ => throwUnsupportedSyntax
 
 declare_syntax_cat temp_binder
@@ -186,71 +185,80 @@ syntax "∀ " : temp_binder
 syntax "exists " : temp_binder
 syntax "∃ " : temp_binder
 
-def elabBinder : Syntax → MetaM Expr
-  | `(temp_binder| forall) | `(temp_binder| ∀) => return .const ``tempBinder.forallBind []
-  | `(temp_binder| exists) | `(temp_binder| ∃)=> return .const ``tempBinder.existsBind []
+def elabBinder : Syntax → MetaM tempBinder
+  | `(temp_binder| forall) | `(temp_binder| ∀) => return .forallBind
+  | `(temp_binder| exists) | `(temp_binder| ∃)=> return .existsBind
   | _ => throwUnsupportedSyntax
 
 -- Represents a "lemma" which is a proposition
-declare_syntax_cat temp_expr
+declare_syntax_cat template
 
-syntax temp_lit " = " temp_lit : temp_expr              -- Equality propositions
-syntax temp_lit : temp_expr                             -- Some literals, can be lemmas (Relational propositions)
+syntax temp_lit " = " temp_lit : template              -- Equality propositions
+syntax temp_lit : template                             -- Some literals, can be lemmas (Relational propositions)
 
-syntax temp_unop temp_expr : temp_expr                  -- Not
-syntax temp_expr temp_binop temp_expr : temp_expr       -- And/Or/Implies
-syntax temp_binder ident ", " temp_expr : temp_expr     -- Forall/Exists
+syntax temp_unop template : template                  -- Not
+syntax template temp_binop template : template       -- And/Or/Implies
+syntax temp_binder ident ", " template : template     -- Forall/Exists
 
-syntax "(" temp_expr ")" : temp_expr                    -- Grouping
+syntax "(" template ")" : template                    -- Grouping
 
-partial def elabExpr : Syntax → MetaM Expr
-  | `(temp_expr| $lit:temp_lit) => do
+partial def tempElab : Syntax → MetaM tempExpr
+  | `(template| $lit:temp_lit) => do
     let e ← elabLit lit
-    if e.isAppOf ``tempLit.opHole then
-      mkAppM ``tempExpr.lit #[e]
-    else
+    match e with
+    | .opHole _ _ =>
+      return .lit e
+    | .var _ =>
       throwErrorAt lit "Only operator applications can be Propositions"
-  | `(temp_expr| $lhs:temp_lit = $rhs:temp_lit) => do
+  --   if e.isAppOf ``tempLit.opHole then
+  --     mkAppM ``tempExpr.lit #[e]
+  --   else
+  --     throwErrorAt lit "Only operator applications can be Propositions"
+  | `(template| $lhs:temp_lit = $rhs:temp_lit) => do
     let lExpr ← elabLit lhs
     let rExpr ← elabLit rhs
-    mkAppM ``tempExpr.eq #[lExpr, rExpr]
-  -- | `(temp_expr| $rel:ident $args:temp_lit_atom*) => do
+    -- mkAppM ``tempExpr.eq #[lExpr, rExpr]
+    return .eq lExpr rExpr
+  -- | `(template| $rel:ident $args:temp_lit_atom*) => do
   --   let nameStr := rel.getId.toString
   --   if nameStr.startsWith "H" then
   --     sorry
   --   else
   --     throwErrorAt rel "Relational holes are operators, must start with 'H' (e.g., H1)"
-  | `(temp_expr| $u:temp_unop $e:temp_expr) => do
+  | `(template| $u:temp_unop $e:template) => do
       let uExpr ← elabUnOp u
-      let eExpr ← elabExpr e
-      mkAppM ``tempExpr.un #[uExpr, eExpr]
-  | `(temp_expr| $lhs:temp_expr $bin:temp_binop $rhs:temp_expr) => do
+      let eExpr ← tempElab e
+      -- mkAppM ``tempExpr.un #[uExpr, eExpr]
+      return .un uExpr eExpr
+  | `(template| $lhs:template $bin:temp_binop $rhs:template) => do
       let binExpr ← elabBinOp bin
-      let lExpr ← elabExpr lhs
-      let rExpr ← elabExpr rhs
-      mkAppM ``tempExpr.bin #[binExpr, lExpr, rExpr]
-  | `(temp_expr| $b:temp_binder $var:ident , $e:temp_expr) => do
+      let lExpr ← tempElab lhs
+      let rExpr ← tempElab rhs
+      -- mkAppM ``tempExpr.bin #[binExpr, lExpr, rExpr]
+      return .bin binExpr lExpr rExpr
+  | `(template| $b:temp_binder $var:ident , $e:template) => do
       let binderExpr ← elabBinder b
       let varIdx ← stxCheck var "x"
-      let eExpr ← elabExpr e
-      mkAppM ``tempExpr.bind #[binderExpr, mkNatLit varIdx, eExpr]
-  | `(temp_expr| ($e:temp_expr)) =>
-    elabExpr e
+      let eExpr ← tempElab e
+      -- mkAppM ``tempExpr.bind #[binderExpr, mkNatLit varIdx, eExpr]
+      return .bind binderExpr varIdx eExpr
+  | `(template| ($e:template)) =>
+    tempElab e
   | _ => throwUnsupportedSyntax
 
-elab "test_tempExpr " e:temp_expr : term => elabExpr e
+-- elab "test_tempExpr " e:template : term => tempElab e
 
-#eval test_tempExpr x1 = H1 x2
-#eval test_tempExpr H1 x1 x2
-#eval test_tempExpr H1 x1 x2 = H1 x2 x1
+-- #eval test_tempExpr x1 = H1 x2
+-- #eval test_tempExpr H1 x1 x2
+-- #eval test_tempExpr H1 x1 x2 = H1 x2 x1
 
--- #eval test_tempExpr ¬ (H1 x1 = x1)
-#eval test_tempExpr (x1 = x2) ∧ (x2 = x3)
-#eval test_tempExpr H1 x1 x2 = x1 ∨ H1 x1 x2 = x2
+-- -- #eval test_tempExpr ¬ (H1 x1 = x1)
+-- #eval test_tempExpr (x1 = x2) ∧ (x2 = x3)
+-- #eval test_tempExpr H1 x1 x2 = x1 ∨ H1 x1 x2 = x2
 
-#eval test_tempExpr ∀ x1, x1 = x1
+-- #eval test_tempExpr ∀ x1, x1 = x1
 
-#eval test_tempExpr ∀ x1,∀ x2, H1 x1 x2 → H2 x1 x2
+-- #eval test_tempExpr ∀ x1, ∃ x2, H1 x1 → H2 x2
 
 /-
 TODO:
